838bd11adff23501d1bd6472185d8bbf2c8d8266
Henry Andrade <henry.andrade@upb.edu.co>
2024-01-31 15:22:27 +0000

    Verifica que el número sea positivo

--- factorial/fact/fact.c
+++ factorial/fact/fact.c
@@ -10,6 +10,11 @@ int main(int argc, char *argv[]) {
     int number = atoi(argv[1]); //Convert the console input argument to an integer
     
     /*It must be verified that the entered number is greater than or equal to zero*/
+    if(number < 0)
+    {
+        perror("El núnmero debe ser mayor a cero");
+        exit(1);
+    }
     
     /*Implement the function that calculates the factorial of the input argument*/
     
